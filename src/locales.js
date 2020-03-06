3dc8cd20508cf0774067d6d4a5299fe917876381
Justin Schroeder <justin@wearebraid.com>
2020-03-06 13:29:19 -0500

    Initial commit of i18n

---
+++ src/locales.js
@@ -0,0 +1,19 @@
+/**
+ * ✏️️ After creating a new locale file (please use en.js as the template) add it
+ * to the list of exports here:
+ */
+
+import da from './locales/da'
+
+import de from './locales/de'
+
+import en from './locales/en'
+
+/**
+ * ✏️ Then add it to the list here:
+ */
+export {
+  da,
+  de,
+  en
+}
---
+++ test/unit/en.test.js
@@ -0,0 +1,29 @@
+import en from '@/locales/en'
+
+// ✏️ import the localized language
+// import de from '@/locales/de'
+
+// ✏️ edit these to be the localized language
+const locale = en
+const localeName = 'en'
+
+describe('German translation', () => {
+  it('exports a function', () => {
+    expect(typeof en).toBe('function')
+  })
+
+  it('calls extend on the formualte instance', () => {
+    const instance = { extends: jest.fn() }
+    en(instance)
+    expect(instance.extends.mock.calls.length).toBe(1)
+  })
+
+  it('includes all the validation results that english does', () => {
+    const instance = { extends: jest.fn() }
+    en(instance)
+    locale(instance)
+    const englishMessages = Object.keys(instance.extends.mock.calls[0][0].locales.en)
+    const localizedMessages = Object.keys(instance.extends.mock.calls[1][0].locales[localeName])
+    expect(englishMessages).toEqual(localizedMessages)
+  })
+})
